4b90a85c3a56b0c56bbcce4108e6a888971a15bb
Nathan Freeman <NathanLFreeman@gmail.com>
2022-03-02 12:06:43 -0500

    Update interface built and working for characters

--- src/App.js
+++ src/App.js
@@ -54,6 +54,23 @@ const App = () => {
     setNewCharacterQuote(char.quote)
   }
 
+  const handleEditFormCancel = () => {
+    setEditFormId('')
+  }
+
+  const handleEditFormSubmit = (char, e) => {
+    e.preventDefault()
+    axios.put(`http://localhost:3000/characters/${char._id}` || `http://stormy-temple-25752.herokuapp.com/characters/${char._id}`, {
+      name: newCharacterName,
+      image: newCharacterImage,
+      quote: newCharacterQuote
+    })
+    .then(() => {
+      updateAllCharacters()
+      setEditFormId('')
+    })
+  }
+
   useEffect(()=> {
     updateAllCharacters()
   }, [])
@@ -68,7 +85,6 @@ const App = () => {
           <li onClick={()=> {setShowNewCharacterForm(!showNewCharacterForm)}}>
           { showNewCharacterForm ? `Cancel` : 
             `Add New Character` }
-          
           </li>
         </ul>
       </header>
@@ -88,15 +104,16 @@ const App = () => {
           {characters.map((char) => {
             return(
               editFormId === char._id ? 
-              <div key={char._id} className="modal">
-                <div className="modal-content">
+              <div key={char._id} className="card edit-card">
+                <div className="edit-card-content">
                   <h2>Edit {char.name}</h2>
-                  <form>
+                  <form onSubmit={(event)=> {handleEditFormSubmit(char, event)}}>
                     Name: <input type="text" value={newCharacterName} onChange={handleNewCharacterName}/><br/>
                     Image URL: <input type="text" value={newCharacterImage} onChange={handleNewCharacterImage}/><br/>
                     Quote: <input type="text" value={newCharacterQuote} onChange={handleNewCharacterQuote}/><br/>
                     <input type="submit" value="Update this character" /><br/>
                   </form>
+                  <button onClick={handleEditFormCancel}>Cancel Edit</button>
                 </div>
               </div>
               :
