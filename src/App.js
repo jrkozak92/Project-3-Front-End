e2610a27d7ac4b3e14b7f36af189c596648636b0
jrkozak92 <jrkozak92@gmail.com>
2022-03-02 13:53:02 -0600

    added s to http

--- src/App.js
+++ src/App.js
@@ -15,14 +15,14 @@ const App = () => {
 
   // Index request
   const updateAllCharacters = () => {
-    axios.get('http://stormy-temple-25752.herokuapp.com/characters').then((response) => {
+    axios.get('https://stormy-temple-25752.herokuapp.com/characters').then((response) => {
       setCharacters(response.data)
     })
   }
 
   const handleNewCharacterFormSubmit = (e) => {
     e.preventDefault()
-    axios.post('http://stormy-temple-25752.herokuapp.com/characters',
+    axios.post('https://stormy-temple-25752.herokuapp.com/characters',
     {
       name: newCharacterName,
       image: newCharacterImage,
@@ -35,7 +35,7 @@ const App = () => {
   }
 
   const handleDeleteCharacter = (char) => {
-    axios.delete(`http://stormy-temple-25752.herokuapp.com/characters/${char._id}`)
+    axios.delete(`https://stormy-temple-25752.herokuapp.com/characters/${char._id}`)
     .then(() => {
       updateAllCharacters()
     })
@@ -73,7 +73,7 @@ const App = () => {
 
   const handleEditFormSubmit = (char, e) => {
     e.preventDefault()
-    axios.put(`http://stormy-temple-25752.herokuapp.com/characters/${char._id}`, {
+    axios.put(`https://stormy-temple-25752.herokuapp.com/characters/${char._id}`, {
       name: newCharacterName,
       image: newCharacterImage,
       quote: newCharacterQuote
